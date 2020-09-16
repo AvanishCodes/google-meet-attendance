f14c673f39c453e43ac995870c7fef00e39e9bff
AvanishCodes <avanishcodes@gmail.com>
2020-09-16 21:43:55 +0530

    Added files to upload on GitHub to make others change it also

--- background.js
+++ background.js
@@ -1,2 +1,10 @@
-alert("The extension is on duty, keep learning");
-// console.log("The extension is on duty, keep learning");
+console.log("Hi, this project is being made and  maintained by AvanishCodes")
+// alert("The extension is on duty, keep learning");
+console.log("The extension is on duty, keep learning");
+
+// Get the names of attendees currently present in the meeting
+sA = document.getElementsByClassName("cS7aqe NkoVdd");
+console.log("Getting the list of attendees");
+for(let j = 0; j < sA.length; j++){
+    console.log(sA[j].innerHTML);
+}
